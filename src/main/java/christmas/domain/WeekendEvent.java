ffeddc144658607bef516010f69512fe7ed1c960
tentenball <lth8905@gmail.com>
2023-11-13 23:01:07 +0900

    [Feat] : 3.3 구현 완료

--- src/main/java/christmas/domain/WeekendEvent.java
+++ src/main/java/christmas/domain/WeekendEvent.java
@@ -1,14 +1,39 @@
 package christmas.domain;
 
-public class WeekendEvent extends Event{
+import christmas.constant.EventConstant;
+import christmas.util.MathUtil;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class WeekendEvent extends Event {
+    private boolean isEnabled;
+    private int reserveDay;
+    private int mainMenuOrderCount;
+    private List<Integer> weekDays = Arrays.asList(EventConstant.FRIDAY, EventConstant.SATURDAY);
+
+    public WeekendEvent(int reserveDate, int mainMenuOrderCount) {
+        this.reserveDay = MathUtil.calculateDayOfWeek(reserveDate);
+        this.mainMenuOrderCount = mainMenuOrderCount;
+    }
+
     @Override
     public Boolean validateEnableEvent() {
-        // TODO Auto-generated method stub
-        return null;
+        if (weekDays.contains(reserveDay)) {
+            isEnabled = true;
+            return true;
+        }
+        isEnabled = false;
+        return false;
     }
+
     @Override
     public int calculateDiscountPrice() {
-        // TODO Auto-generated method stub
-        
+        return EventConstant.WEEK_EVENT_DISCOUNT_PRICE * mainMenuOrderCount;
+    }
+
+    @Override
+    public Boolean getIsEnabled(){
+        return isEnabled;
     }
 }
