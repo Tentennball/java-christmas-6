7922c5a582591a951f094f95002cbcf1b815d69b
tentenball <lth8905@gmail.com>
2023-11-13 23:01:29 +0900

    [Feat] : 3.4 구현완료

--- src/main/java/christmas/domain/SpecialEvent.java
+++ src/main/java/christmas/domain/SpecialEvent.java
@@ -1,14 +1,35 @@
 package christmas.domain;
 
-public class SpecialEvent extends Event{
+import christmas.constant.EventConstant;
+import christmas.util.MathUtil;
+
+public class SpecialEvent extends Event {
+    private boolean isEnabled;
+    private int reserveDate;
+    private int reserveDay;
+
+    public SpecialEvent(int reserveDate) {
+        this.reserveDate = reserveDate;
+        this.reserveDay = MathUtil.calculateDayOfWeek(reserveDate);
+    }
+
     @Override
     public Boolean validateEnableEvent() {
-        // TODO Auto-generated method stub
-        return null;
+        if (reserveDay % 7 == EventConstant.SUNDAY || reserveDate == EventConstant.CHRISTMAS_DAY){
+            isEnabled = true;
+            return true;
+        }
+        isEnabled = false;
+        return false;
     }
+
     @Override
     public int calculateDiscountPrice() {
-        // TODO Auto-generated method stub
-        
+        return EventConstant.SPECIAL_EVENT_DISCOUNT_PRICE;
+    }
+
+    @Override
+    public Boolean getIsEnabled(){
+        return isEnabled;
     }
 }
